bd25559858e8732737d333121d9c02f6d3cf5f0f
haokaili <>
2021-05-24 03:03:35 +0000

    finish

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ICS2O-Unit5-03-GO-Age-and-Movie
+[![GitHub's Super Linter](https://github.com/haokai-li/ICS2O-Unit5-03-GO-Age-and-Movie/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/haokai-li/ICS2O-Unit5-03-GO-Age-and-Movie/actions)
---
+++ ageInput.go
@@ -0,0 +1,38 @@
+// Created by: haokai
+// Created on: May 2021
+// This program displays, "Age-and-Movie"
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// This function does addition
+	var age int
+
+	// input
+	fmt.Println("This program is about age limit of movie .")
+	fmt.Println()
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
+
+	// detect
+	if age >= 17 {
+		// output of right answer
+		fmt.Println("You can see a R movie aline.")
+	} else if age >= 13 {
+		// output of wrong answer
+		fmt.Println("You can see a PG-13 movie aline.")
+	} else if age >= 5 {
+		// output of wrong answer
+		fmt.Println("You can see a G or PG movie aline.")
+	} else {
+		// output of wrong answer
+		fmt.Println("Uh, you are too young for most things.")
+	}
+	fmt.Println("Thanks for verifying  your age!")
+	fmt.Println("\nDone.")
+}
